295abc396b271bb94a37859e7fb210171a656e2e
Asem Aly <eng.asem.aly@hotmail.com>
2023-07-20 21:16:56 +0300

    Selection Sort

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+List<char> unSortedData = new List<char>() { 'C', 'F', 'D', 'Z', 'A', 'M', 'S', 'G'};
+Console.WriteLine("Unsorted List:");
+printList(unSortedData);
+Console.WriteLine("After sorted: ");
+selectioSort(unSortedData);
+
+
+static void selectioSort(List<char> array)
+{
+    for (int i = 0; i < array.Count; i++)
+    {
+        var minIndex = i;
+        for (int j = i + 1; j < array.Count; j++)
+        {
+            if (array[j] < array[minIndex])
+            {
+                minIndex = j;
+            }
+           
+        }
+        char temp = array[minIndex];
+        array[minIndex] = array[i];
+        array[i] = temp;
+
+    }
+    printList(array);
+}
+
+static void printList(List<char> array)
+{
+    foreach (var item in array)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+
