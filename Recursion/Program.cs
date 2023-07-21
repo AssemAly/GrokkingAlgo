07846c0aca5561ecee2be30ca4c40df00d6c9362
Asem Aly <eng.asem.aly@hotmail.com>
2023-07-21 12:19:38 +0300

    Recusion Info

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -5,6 +5,11 @@
  * 1) Recusive Case -> which function call itself 
  * 2) Base Case -> stop poit which say we all done 
  * Base case very important to prevent recusion from going to infite loop
+ * It save in memery as stack
+ * Using stack has cost, saving all info can take alot of memory 
+ * if stack is too tall it may consume alot of memory space in this case we have two options
+ * 1) Rewrite code using loop instead
+ * 2) Use advanced recusrion called tail recusion, but not supported by all languages
  */
 Console.Write("Enter number to get it's factorial: ");
 int number  = Convert.ToInt32(Console.ReadLine());
